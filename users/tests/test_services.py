e02b5c3aae425846def8c4090d89d8e8b60cd8a2
rodrigondec <rodrigondec@gmail.com>
2020-04-25 16:49:47 -0300

    service test update password

--- users/tests/test_services.py
+++ users/tests/test_services.py
@@ -1,17 +1,30 @@
 from ..services import UserService
 from ..models import User
+from ..factories import UserFactory
 from core.test_utils import ImageTestCaseMixin
 
 
 class UserServiceTestCase(ImageTestCaseMixin):
     def test_create(self):
-        user = UserService.create(email="foo@bar.com", picture=self.image_name)
+        user = UserService.create(email="foo@bar.com", password='senha', picture=self.image_name)
 
         self.assertIsInstance(user, User)
+        self.assertEqual(user.email, 'foo@bar.com')
+        self.assertTrue(user.has_usable_password())
         self.assertEqual(user.picture.name, self.image_name)
         self.assertTrue(user._picture.url)
 
-    def test_update(self):
+    def test_update_password(self):
+        user = UserFactory()
+
+        self.assertIsInstance(user, User)
+        self.assertFalse(user.has_usable_password())
+
+        user = UserService.update(user.id, password='nova senha')
+
+        self.assertTrue(user.has_usable_password())
+
+    def test_update_picture(self):
         user = UserService.create(email="foo@bar.com")
 
         self.assertIsInstance(user, User)
